5d40d241b88f6b9a100766f1147545269384deae
liaoipeng <liaoipeng@icloud.com>
2025-12-16 11:09:06 +0800

    Initial Commit

---
+++ final/finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by 廖逸芃 on 2025/12/16.
+//
+
+import SwiftUI
+
+@main
+struct finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
